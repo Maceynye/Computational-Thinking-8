6e190dedfc844e8445418e45d1a3c11444e6f5aa
Maceynye <maceybuesing@seattleacademy.org>
2026-04-08 21:49:29 +0000

    project2

--- project2.py
+++ project2.py
@@ -16,7 +16,7 @@ elif answer1 == "D":
 
 
 answer2 = input ("Do you perfer A worm  wether, B cold wether, C Hot weather, or D rainy weather?  ")
-if answer2 == "A"
+if answer2 == "A":
         spring_points += 1
 elif answer2 == "B":
         fall_points += 1
@@ -26,7 +26,7 @@ elif answer2 == "D":
     winter_points += 1 
 
 
-answer3 = input (" would you rather A hike, B ski, C swim, D, stay in side  ")
+answer3 = input (" would you rather A hike, B ski, C swim,or D stay in side  ")
 if answer3 == "A" or answer3 == "B" :
     spring_points += 1
 elif answer3 == "C" :
@@ -36,7 +36,7 @@ elif answer3 == "D":
     winter_points += 1 
 
     print (f"Your score is { spring_points} winter, {fall_points} fall, {summer_points} summer, and {winter_points} winter")
-elif spring_ponts >fall_points and spring_points > summer_points:
+if spring_points >fall_points and spring_points > summer_points:
      print(" you are a spring person")
 elif winter_points > spring_points and winter_points > summer_points:
      print(" this shows your a winter person")
